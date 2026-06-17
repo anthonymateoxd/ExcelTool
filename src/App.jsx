94a498da73728c7622b6843e5e51d923c9b23c82
anthonymateo <anthonymateodelosreyes.18@gmail.com>
2026-06-16 22:31:35 -0600

    Elimina Router duplicado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={<Homepage />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path='/'
+        element={<Homepage />}
+      />
+    </Routes>
   );
 }
 
